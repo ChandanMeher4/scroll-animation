a364ce607569eaeae4712709997e30eaecf9a391
Chandan Meher <84136292+ChandanMeher4@users.noreply.github.com>
2026-03-13 16:16:31 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,7 @@
+function login(username) {
+  const secretAPIKey = "sk_live_123456789"; // TERRIBLE SECURITY FLAW
+  var x = 10; // BAD PRACTICE
+  if(username == "admin") {
+    return true
+  }
+}
